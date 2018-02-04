bf81f6a9a56cce7b28f8eab94e44fa51a6ec05bf
dom.soesatyo@gmail.com <dom.soesatyo@gmail.com>
2018-02-04 18:47:49 +0000

    Added create action to save to db

--- db/migrate/20180202183836_create_contacts.rb
+++ db/migrate/20180202183836_create_contacts.rb
@@ -1,6 +1,6 @@
 class CreateContacts < ActiveRecord::Migration[5.0]
   def change
-    create_table :contacts do |t|
+    create_table :contacts do |t|cd s
       t.string :name
       t.string :email
       t.text :comments
